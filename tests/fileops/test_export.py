7ee6865a814a1a6c1220838338c748ab8baddfe2
M.Benninghoff <mb.social@posteo.de>
2021-11-26 21:31:06 +0200

    relative paths

--- tests/fileops/test_export.py
+++ tests/fileops/test_export.py
@@ -10,19 +10,19 @@ from cruisetrack.fileops.rtz_export import rtz_export
 class TestExportFunctions(TestCase):
 
     def setUp(self) -> None:
-        dbfile = open('/home/markus/scripting/cruise_track/tests/data/lat_lon_sixpoints', 'rb')
+        dbfile = open('../data/lat_lon_sixpoints', 'rb')
         db = pickle.load(dbfile)
 
         self.lat = db["lat"]
         self.lon = db["lon"]
 
     def test_rt3_export(self):
-        rt3_export(self.lon, self.lat, filename='/home/markus/scripting/cruise_track/tests/data/rt3_test.rt3')
+        rt3_export(self.lon, self.lat, filename='../data/rt3_test.rt3')
 
     def test_rtz_export(self):
-        rtz_export(self.lon, self.lat, filename='/home/markus/scripting/cruise_track/tests/data/rtz_test.rtz')
+        rtz_export(self.lon, self.lat, filename='../data/rtz_test.rtz')
 
     def test_cvt_export(self):
-        csv_export(self.lon, self.lat, filename='/home/markus/scripting/cruise_track/tests/data/cvt_test.cvt')
+        csv_export(self.lon, self.lat, filename='../data/cvt_test.cvt')
 
 
