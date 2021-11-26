c191b5c56097d7ee8a1e1462005639d36ff20158
M.Benninghoff <mb.social@posteo.de>
2021-11-26 22:45:16 +0200

    Added Typing and Docstrings

--- cruisetrack/process/workflow_points.py
+++ cruisetrack/process/workflow_points.py
@@ -7,15 +7,26 @@ from qgis.core import QgsVectorLayer
 from cruisetrack.process.tsp_nn import tsp_nn
 
 
-def point_workflow(laye_r: QgsVectorLayer):
-    """run through stations most efficiently (traveling salesman problem approach)"""
+def point_workflow(laye_r: QgsVectorLayer) -> Tuple[List, List]:
+    """
+    Point workflow utilizing Traveling Salesman Problem Solver for creating shortest track.
+
+    :param laye_r: QgsVectorLayer (Point)
+    :return: Tuple of Lat and Lon lists
+    """
     df = point_layer_to_df(laye_r)
     lon, lat = calc_lat_lon(df)
     plot_track(lon, lat)
     return lon, lat
 
 
-def point_layer_to_df(laye_r) -> pd.DataFrame:
+def point_layer_to_df(laye_r: QgsVectorLayer) -> pd.DataFrame:
+    """
+    Convert QgsVectorLayer to DataFrame
+
+    :param laye_r: QgsVectorLayer (Point)
+    :return: DataFrame containing X, Y coordinates
+    """
     row_list = []
     for feat in laye_r.getFeatures():
         row_list.append({'X': feat.geometry().asPoint().x(),
@@ -24,7 +35,13 @@ def point_layer_to_df(laye_r) -> pd.DataFrame:
     return df
 
 
-def calc_lat_lon(df: pd.DataFrame) -> Tuple[List, List]:
+def calc_lat_lon(df: pd.DataFrame) -> Tuple[List[float], List[float]]:
+    """
+    Create stations based on input point, using Traveling Salesman Problem Solver.
+
+    :param df: DataFrame containing X, Y coordinates
+    :return: Tuple of Lat and Lon lists
+    """
     station_order = tsp_nn(df)
     station_order = station_order.tolist()
 
@@ -35,7 +52,14 @@ def calc_lat_lon(df: pd.DataFrame) -> Tuple[List, List]:
     return lon, lat
 
 
-def plot_track(lon, lat):
+def plot_track(lon: List[float], lat: List[float]):
+    """
+    Quick Matplotlib plot.
+
+    :param lon: List of Floats
+    :param lat: List of Floats
+    :return:
+    """
     # plot the track to check the track
     plt.figure(4)
     plt.plot(lon, lat, label="track")
