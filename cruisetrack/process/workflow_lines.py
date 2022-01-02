435ca4410047e334dfcee9203cc00749c1051ad1
M.Benninghoff <mb.social@posteo.de>
2022-01-02 17:22:48 +0200

    added test csv and pickle file for parallel lines workflow.

--- cruisetrack/process/workflow_lines.py
+++ cruisetrack/process/workflow_lines.py
@@ -124,7 +124,7 @@ def lines_workflow(layer_provider, laye_r, is_individual_trackline, is_accessory
                  'is_normal_profile': is_normal_profile, 'flip_we' : flip_we,
                  'only_process_2nds': only_process_2nds, 'is_littorina': is_littorina,
                  'flip_ns': flip_ns},
-                '/home/markus/scripting/cruise_track/tests/data/line_layer_as_df.pickle')
+                '/home/markus/scripting/cruise_track/tests/data/parallel_lines_as_df.pickle')
     lon, lat = process_lines(df, is_individual_trackline, is_accessory, is_nonebt,
                    is_normal_profile, flip_we, only_process_2nds, is_littorina, flip_ns)
     plot_track(lon, lat, label='start')
